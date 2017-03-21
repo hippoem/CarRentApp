9e193dcd47b76f071e743406eed302e8b7ed6bae
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-21 15:08:50 +0700

    test List Show

--- scenes/Search.js
+++ scenes/Search.js
@@ -20,12 +20,35 @@ export default class Search extends Component {
   renderRow(rowData){
     return(
       <ListItem>
-        <Body>
-          <Text>{rowData.title}</Text>
-        </Body>
-        <Right>
-          <Icon name="arrow-forward"/>
-        </Right>
+        <Card>
+            <CardItem>
+                <Left>
+                    <Thumbnail source={require('../img/2017-lamborghini-aventador-s-5.jpg')}/>
+                    <Body>
+                        <Text>NativeBase</Text>
+                        <Text note>GeekyAnts</Text>
+                    </Body>
+                </Left>
+              </CardItem>
+              <CardItem cardBody>
+                  <Image style={{ resizeMode: 'cover' }} source={require('../img/pexels-photo-300001.jpeg')} />
+              </CardItem>
+              <CardItem content>
+                  <Text>{rowData.title}</Text>
+              </CardItem>
+              <CardItem style={{ justifyContent: 'space-around' }}>
+                  <Button transparent>
+                      <Icon active name="thumbs-up" />
+                      <Text>12 Likes</Text>
+                  </Button>
+                  <Button transparent>
+                      <Icon active name="chatbubbles" />
+                      <Text>4 Comments</Text>
+                  </Button>
+
+            </CardItem>
+       </Card>
+
       </ListItem>
     );
   }
@@ -53,36 +76,6 @@ export default class Search extends Component {
                       dataSource={this.state.dataSource}
                       renderRow={this.renderRow.bind(this)}
                     />
-                    <Card >
-                        <CardItem>
-                            <Left>
-                                <Thumbnail source={require('../img/2017-lamborghini-aventador-s-5.jpg')}/>
-                                <Body>
-                                    <Text>NativeBase</Text>
-                                    <Text note>GeekyAnts</Text>
-                                </Body>
-                            </Left>
-                          </CardItem>
-                          <CardItem cardBody>
-                              <Image style={{ resizeMode: 'cover' }} source={require('../img/pexels-photo-300001.jpeg')} />
-                          </CardItem>
-                          <CardItem content>
-                              <Text>Wait a minute. Wait a minute, Doc. Uhhh...
-                              Are you telling me that you built a time machine... out of a DeLorean?!
-                              Whoa. This is heavy.</Text>
-                          </CardItem>
-                          <CardItem style={{ justifyContent: 'space-around' }}>
-                              <Button transparent>
-                                  <Icon active name="thumbs-up" />
-                                  <Text>12 Likes</Text>
-                              </Button>
-                              <Button transparent>
-                                  <Icon active name="chatbubbles" />
-                                  <Text>4 Comments</Text>
-                              </Button>
-
-                        </CardItem>
-                   </Card>
                 </Content>
             </Container>
         );
