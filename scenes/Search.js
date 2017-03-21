e44f01ba5c6a3b4a2846d019a0bfe2553d1933b7
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-21 14:20:50 +0700

    test Search

--- scenes/Search.js
+++ scenes/Search.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View } from 'react-native';
-import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
+import {StyleSheet, View } from 'react-native';
+import { Image } from 'react-native';
+import { Container, Content, Card, CardItem, Left, Body, Thumbnail, Text, Button, Icon } from 'native-base';
 
 export default class Search extends Component {
 
   render() {
-    return(
-      <Container>
-      <Body>
-        <Text>Header Page</Text>
-      </Body>
-      </Container>
-    );
-  }
+        return (
+            <Container>
+                <Content>
+                    <Card >
+                        <CardItem>
+                            <Left>
+                                <Thumbnail source={require('../img/2017-lamborghini-aventador-s-5.jpg')}/>
+                                <Body>
+                                    <Text>NativeBase</Text>
+                                    <Text note>GeekyAnts</Text>
+                                </Body>
+                            </Left>
+                          </CardItem>
+                          <CardItem cardBody>
+                              <Image style={{ resizeMode: 'cover' }} source={require('../img/pexels-photo-300001.jpeg')} />
+                          </CardItem>
+                          <CardItem content>
+                              <Text>Wait a minute. Wait a minute, Doc. Uhhh...
+                              Are you telling me that you built a time machine... out of a DeLorean?!
+                              Whoa. This is heavy.</Text>
+                          </CardItem>
+                          <CardItem style={{ justifyContent: 'space-around' }}>
+                              <Button transparent>
+                                  <Icon active name="thumbs-up" />
+                                  <Text>12 Likes</Text>
+                              </Button>
+                              <Button transparent>
+                                  <Icon active name="chatbubbles" />
+                                  <Text>4 Comments</Text>
+                              </Button>
+                              
+                        </CardItem>
+                   </Card>
+                </Content>
+            </Container>
+        );
+    }
 
 }
